c897865540663f9b3312c41794046b3a5f93b5f7
shivanisolanki <shivanisolanki9691@gmail.com>
2023-03-25 19:41:10 +0530

    first commit

--- db/migrate/20230324093347_create_todos.rb
+++ db/migrate/20230324093347_create_todos.rb
@@ -1,6 +1,6 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
-    create_table :todos do |t|
+    create_table (:todos )do |t|
       t.string :name
       t.string :day
       t.string :email
