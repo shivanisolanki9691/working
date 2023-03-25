86fb01192a1cf0856e62c6ad495c8793d00bc029
shivanisolanki <shivanisolanki9691@gmail.com>
2023-03-25 17:26:08 +0530

    update app to render

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,3 @@
 class WelcomeController < ApplicationController
-  def index
-    @monday_todos = Todo.where(day: 'Monday')
-    @tuesday_todos = Todo.where(day: 'Tuesday')
-    @wednesday_todos = Todo.where(day: 'Wednesday')
-    @thuesday_todos = Todo.where(day: 'Thuesday')
-    @friday_todos = Todo.where(day: 'Friday')
-    @saturday_todos = Todo.where(day: 'saturday')   
-    @sunday_todos = Todo.where(day: 'sunday')
-  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 
 Rails.application.routes.draw do
   # get 'welcome/index''
-  root 'todos#index'
+  root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :todos
   resources :users
