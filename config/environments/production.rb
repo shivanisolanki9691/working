3052e160102fd3f463204231315e299c4e83c0f5
shivanisolanki <shivanisolanki9691@gmail.com>
2023-03-27 14:21:33 +0530

    changes in database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,9 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { :host => "dpg-cge25sqk728lv80g24ug-a"}
 
   config.active_record.dump_schema_after_migration = false
