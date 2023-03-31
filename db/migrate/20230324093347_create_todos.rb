7092806a0652f890f4755b58c0faab93c13b74a9
shivanisolanki <shivanisolanki9691@gmail.com>
2023-03-31 11:57:46 +0530

    deploy

--- db/migrate/20230324093347_create_todos.rb
+++ db/migrate/20230324093347_create_todos.rb
@@ -1,11 +1,13 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
-    create_table :todos do |t|
-      t.string :name
-      t.string :day
-      t.string :email
+    unless table_exists?(:todos)
+      create_table :todos do |t|
+        t.string :name
+        t.string :day
+        t.string :email
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
